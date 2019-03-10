007a2515f006f50839e2171f87756dc252061625
JGray <judithannegray@gmail.com>
2019-03-10 18:31:30 +0000

    Added stub file with no solutions for practice, added get evens from a list solution to main file.

--- recursion_stub.py
+++ recursion_stub.py
@@ -29,10 +29,7 @@ def range_sum(begin, end, step):
     5
 
     """
-    if end - begin < 0:
-        return 0
-
-    return begin + range_sum(begin + step, end, step)
+    pass
 
 def sum_nested(lst):
     """
@@ -43,21 +40,7 @@ def sum_nested(lst):
     6
         
     """
-    # output = 0
-    # for item in lst:
-    #     output += item if not isinstance(item, list) else sum_nested(item)
-
-    # return output
-
-    if len(lst) == 0:
-        return 0
-    else:
-        head = lst[0]
-        if isinstance(head, int):
-            return head + sum_nested(lst[1:])
-        else:
-            return sum_nested(head) + sum_nested(lst[1:])
-
+    pass
 
 def stringify(node):
     """ 
@@ -72,14 +55,7 @@ def stringify(node):
     'None'
 
     """
-    output = ""
-    if not node:
-        return output + "None"
-    return output + str(node.data) + " -> " + stringify(node.next)
-
-    # if not node:
-    #     return 'None'
-    # return f'{node.data} -> {stringify(node.next)}'
+    pass
 
 
 def find_max(lst):
@@ -106,14 +82,7 @@ def find_max(lst):
 
 
     """
-    if len(lst) == 1:
-        return lst[0]
-    else:
-        highest = find_max(lst[1:])
-        if lst[0] > highest:
-            return lst[0]
-        else:
-            return highest
+    
 
 def print_list(lst):
     """Prints out each element in a list recursively.
@@ -129,11 +98,7 @@ def print_list(lst):
     3
 
     """
-    if len(lst) == 1:
-        print(lst[0])
-    else:
-        print(lst[0])
-        return print_list(lst[1:])
+    
 
 
 def print_integers(n):
@@ -151,9 +116,7 @@ def print_integers(n):
     2
 
     """
-    if n >= 1:
-        print_integers(n-1)
-        print(n)
+    
 
 
 def double_list(lst):
@@ -166,19 +129,8 @@ def double_list(lst):
     [4, 4, 4]
 
     """
-    # new_lst = []
-    # for n in lst:
-    #     if isinstance(n, list):
-    #         print_doubled_integers(n)
-    #     else:
-    #         new_lst.append(n*2)
-    # return new_lst
-
-    #no iteration version
-    if len(lst) == 0:
-        return []
-    else:
-        return[lst[0]*2] + double_list(lst[1:])
+    
+    
 
 def get_factorial(n):
     """Return the factorial of a given integer
@@ -190,12 +142,6 @@ def get_factorial(n):
     24
 
     """
-
-    if n == 1:
-        return n
-
-    return n * get_factorial(n-1)
-
 def get_even(lst):
     """Return a list of even integers, given a list of integers
 
@@ -209,18 +155,8 @@ def get_even(lst):
     []
 
     """
-    if len(lst) == 0:
-        return []
-    else:
-        if lst[0] % 2 == 0:
-            return [lst[0]] + get_even(lst[1:])
-        else:
-            return get_even(lst[1:])
-
-    # return [item for item in lst if item % 2 == 0]
-
+    
 if __name__ == "__main__":
     import doctest
     if doctest.testmod().failed == 0:
         print("\n*** ALL TESTS PASSED. YAY!\n")
-
