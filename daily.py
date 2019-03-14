83613647d679527a0fc29c333bf178685f4264f7
JGray <judithannegray@gmail.com>
2019-03-14 19:42:29 +0000

    Added get product less the element at index function.

--- daily.py
+++ daily.py
@@ -1,7 +1,8 @@
-
+from functools import reduce
+import operator
 
 def does_sum(lst, n):
-    """Returns whether any two numbers in a given list sum to a given num
+    """Return whether any two numbers in a given list sum to a given num
 
     >>> does_sum([10, 15, 3, 7], 17)
     True
@@ -16,7 +17,33 @@ def does_sum(lst, n):
             return True
     return False
 
+def prod(lst):
+    """
+    >>> prod([1, 2, 3])
+    6
 
+    >>> prod([2, 3, 4])
+    24
+    """
+    
+    return reduce(operator.mul, lst, 1)
+
+def get_product_less_idx_element(lst):
+    """Return an array with each element at index i is the product of all the numbers
+    except the one at i in a given array
+
+    >>> get_product_less_idx_element([1, 2, 3, 4, 5])
+    [120, 60, 40, 30, 24]
+
+    >>> get_product_less_idx_element([3, 2, 1])
+    [2, 3, 6]
+    """
+
+
+    return [int(prod(lst)/num) for num in lst]
+
+
+    
 
 
 
